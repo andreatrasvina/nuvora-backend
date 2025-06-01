6a9f22fa414d3f1d169c75a16e4f075e19bd6b5e
Andrea Lucero <alucerotrasvina@gmail.com>
2025-05-31 19:22:32 -0700

    client message

--- server/index.js
+++ server/index.js
@@ -4,17 +4,24 @@ import logger from 'morgan';
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
 
-const port = process.env.PORT ?? 3001;
+const port = process.env.PORT ?? 3000;
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const app = express(); //manejador de rutas http
+const server = createServer(app); //el servidor http real, si hay peticion la manda a express para que la maneje
+const io = new Server(server); //convierte el protocolo http a la conexion websocket despues del handshake
 
+//responde a la accion cuando un usuario se ha conectado
 io.on('connection', (socket) => {
     console.log('a user has connected!!');
     
+    //responde a la accion cuando un usuario se ha desconectado
     socket.on('disconnect', () => {
         console.log('an user has disconnected');
+    });
+
+    //responde a la accion cuando un usuario envia un mensaje
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg)
     });
 });
 
