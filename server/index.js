ebc3c2fba1cd5d82b88866ff682fbbc80c76eb6e
Andrea Lucero <alucerotrasvina@gmail.com>
2025-05-31 20:18:15 -0700

    persistent message

--- server/index.js
+++ server/index.js
@@ -11,13 +11,13 @@ dotenv.config();
 const port = process.env.PORT ?? 3000;
 
 const app = express(); //manejador de rutas http
-const server = createServer(app); //el servidor http real, si hay peticion la manda a express para que la maneje
+const server = createServer(app); //el servidor http real, si hay peticion la manda a express para que el las maneje
 const io = new Server(server, { //convierte el protocolo http a la conexion websocket despues del handshake
     connectionStateRecovery: {}
 }); 
 
 const db = createClient({
-    url: "libsql://primary-rainmaker-andreatrasvina.aws-us-west-2.turso.io",
+    url: "libsql://flowing-warbird-andreatrasvina.aws-us-west-2.turso.io",
     authToken: process.env.DB_TOKEN,
 });
 
@@ -29,7 +29,7 @@ await db.execute(`
 `);
 
 //responde a la accion cuando un usuario se ha conectado
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('a user has connected!!');
     
     //responde a la accion cuando un usuario se ha desconectado
@@ -55,6 +55,22 @@ io.on('connection', (socket) => {
         console.log('message: ' + msg); //para verlos aki cerquita jeje
         io.emit('chat message', msg, result.lastInsertRowid.toString());
     });
+
+    if(!socket.recovered){
+        try {
+            const results = await db.execute({
+                sql: 'SELECT id, content FROM messages WHERE id > ?',
+                args: [socket.handshake.auth.serverOffset ?? 0]
+            });
+
+            results.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString());
+            });
+            
+        } catch (error) {
+            console.error(e);
+        }
+    }
 });
 
 app.use(logger('dev'));
