62fdc17a1ef84522d73290873ea75961e9e649e8
Andrea Lucero <alucerotrasvina@gmail.com>
2025-05-31 19:05:14 -0700

    morgan

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import logger from 'morgan';
 
 const port = process.env.PORT ?? 3001;
 
 const app = express();
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
     res.send('<h1>CHAT. This is my chat!!!</h1>');
