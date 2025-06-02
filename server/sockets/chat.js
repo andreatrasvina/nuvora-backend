5bc28568fadf9e20a06116df9ff88abc27d5095a
MarcosIgnacioo <magma_ruby145@hotmail.com>
2025-06-02 15:47:53 -0700

    adapting stuff to the rooms schema

--- server/sockets/chat.js
+++ server/sockets/chat.js
@@ -7,31 +7,18 @@ const users = {}
 export function setupChat(io) {
   io.on('connection', async (socket) => {
     console.log('a user has connected!!');
-    const roomID = socket.handshake.auth.room_id
+    const roomID = socket.handshake.auth.room_id ?? 1
+    console.log(roomID);
     socket.join(roomID)
 
     //responde a la accion cuando un usuario se ha desconectado
     socket.on('disconnect', () => {
+      socket.leave(roomID)
       console.log('an user has disconnected');
     });
 
     //responde a la accion cuando un usuario envia un mensaje
-    socket.on('chat message', async (msg) => {
-      let result;
-      try {
-        result = await db.execute({
-          sql: 'INSERT INTO messages (content) VALUES (:msg)',
-          args: { msg }
-        });
-      } catch (e) {
-        console.error(e);
-        return;
-      }
-      console.log('message: ' + msg); //para verlos aki cerquita jeje
-      io.emit('chat message', msg, result.lastInsertRowid.toString()); //c propagan a todos los usuarios
-    });
-
-    socket.on('chat room', async (msg_wrapper) => {
+    socket.on('send message', async (msg_wrapper) => {
       let result
       try {
         result = await db.execute({
@@ -42,14 +29,18 @@ export function setupChat(io) {
         console.error(e);
         return;
       }
-      console.log('message: ' + msg_wrapper.msg); //para verlos aki cerquita jeje
-      io.to(roomID).emit('chat message', msg_wrapper.msg, result.lastInsertRowid.toString()); //c propagan a todos los usuarios
+      console.log('room message: ' + msg_wrapper.msg); //para verlos aki cerquita jeje
+      io.to(roomID).emit(
+        'chat message',
+        msg_wrapper.msg,
+        result.lastInsertRowid.toString()
+      ); //c propagan a todos los usuarios
+
     });
 
     //por si acaso.. recupera el msj
     if (!socket.recovered) {
       try {
-        console.log(roomID);
         const results = await db.execute({
           sql: 'SELECT id, content FROM messages WHERE id > ? AND room_id = ?',
           args: [socket.handshake.auth.serverOffset ?? 0, roomID]
