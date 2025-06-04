51e864d1ef52e2dad180683f1d1261d88ddb0ac1
Andrea Lucero <alucerotrasvina@gmail.com>
2025-06-03 22:45:13 -0700

    timestamp

--- server/sockets/chat.js
+++ server/sockets/chat.js
@@ -25,6 +25,8 @@ export function setupChat(io) {
       const media64 = Buffer.from(media).toString('base64')
       msg_wrapper.media = media64
 
+      let messageTimestamp;
+
       try {
         result = await db.execute({
           sql: 'INSERT INTO messages (room_id, content, media, mime_type, user_id) VALUES (:room_id, :msg, :media, :mime_type, :user_id)',
@@ -36,16 +38,29 @@ export function setupChat(io) {
             user_id: user_id
           }
         });
+
+        const insertedMessage = await db.execute({
+          sql: 'SELECT timestamp FROM messages WHERE id = ?',
+          args: [result.lastInsertRowid],
+        });
+        messageTimestamp = insertedMessage.rows[0].timestamp;
+
       } catch (e) {
         console.error(e);
         return;
       }
 
-
       console.log('room message: ' + msg, media); //para verlos aki cerquita jeje
       io.to(roomID).emit(
         'chat message',
-        msg_wrapper,
+        { 
+          msg: msg, 
+          media: media64, 
+          mime_type: mime_type, 
+          user_id: user_id, 
+          name: name,       
+          timestamp: messageTimestamp 
+        },
         result.lastInsertRowid.toString()
       ); //c propagan a todos los usuarios
 
@@ -57,17 +72,25 @@ export function setupChat(io) {
         const query = `SELECT messages.id, messages.content, messages.media, messages.mime_type, messages.user_id FROM messages WHERE id > ? AND room_id = ?`
         const results = await db.execute({
           sql: `
-          SELECT messages.id, messages.content, messages.media, messages.mime_type, messages.user_id, users.id, users.name
+          SELECT messages.id, messages.content, messages.media, messages.mime_type, messages.user_id, messages.timestamp, users.id, users.name
           FROM messages
           FULL OUTER JOIN users ON users.id=messages.user_id
-          WHERE messages.id > ? AND messages.room_id = ?;`
+          WHERE messages.id > ? AND messages.room_id = ?
+          ORDER BY messages.timestamp ASC;`
           ,
           args: [socket.handshake.auth.serverOffset ?? 0, roomID]
         });
         results.rows.forEach(row => {
           // const media = Buffer.from(row.media).toString('base64');
           const media = row.media
-          socket.emit('chat message', { user_id: row.user_id, name: row.name, media: media, msg: row.content, mime_type: row.mime_type }, row.id.toString());
+          socket.emit('chat message', 
+            { user_id: row.user_id, 
+              name: row.name, 
+              media: media, 
+              msg: row.content, 
+              mime_type: row.mime_type,
+              timestamp: row.timestamp 
+            }, row.id.toString());
         });
       } catch (e) {
         console.error(e);
