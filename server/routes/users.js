29ba9b831b17760f66e622dc00bddb9f925318a6
Andrea Lucero <alucerotrasvina@gmail.com>
2025-06-03 18:17:30 -0700

    profile picture update

--- server/routes/users.js
+++ server/routes/users.js
@@ -56,4 +56,61 @@ router.get('/', async (req, res) => {
   }
 });
 
+//update
+router.put('/update/:id', async (req, res) => {
+  const { name, email, password, profile_picture } = req.body;
+  const { id } = req.params;
+
+  try {
+    const fields = { name, email };
+
+    const updates = [];
+    const values = [];
+
+    //foto de perfil
+    if (profile_picture) {
+      const cleanBase64 = profile_picture.replace(/^data:image\/\w+;base64,/, '');
+      const base64Encoded = Buffer.from(cleanBase64, 'base64').toString('base64');
+      fields.profile_picture = base64Encoded;
+    }
+
+    //texto
+    for (const [key, value] of Object.entries(fields)) {
+      if (value) {
+        updates.push(`${key} = ?`);
+        values.push(value);
+      }
+    }
+
+    //contarsena
+    if (password) {
+      const hashed = await bcrypt.hash(password, 10);
+      updates.push('password = ?');
+      values.push(hashed);
+    }
+
+    //envia algo
+    if (updates.length === 0) {
+      return res.status(400).json({
+        message: 'No actualizaste nada.'
+      });
+    }
+
+    const sql = `UPDATE users SET ${updates.join(', ')} WHERE id = ?`;
+    values.push(id);
+
+    await db.execute({ sql, args: values });
+
+    res.status(200).json({
+      message: 'Usuario actualizado.'
+    });
+
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      message: 'Error al actualizae.'
+    });
+  }
+});
+
 export default router;
